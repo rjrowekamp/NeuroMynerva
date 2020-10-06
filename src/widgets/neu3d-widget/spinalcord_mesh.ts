91919813544808b4ffac9664c71b029d19f65d4d
rjrowekamp <rrowekamp@salk.edu>
2020-10-06 13:41:58 -0700

    Updated spinalcord_mesh.ts with new githack links.

--- src/widgets/neu3d-widget/spinalcord_mesh.ts
+++ src/widgets/neu3d-widget/spinalcord_mesh.ts
@@ -2,9 +2,9 @@
  * Exports only the mesh required to render spinalcord
  */
 export var SpinalcordMesh = {
-    "WhiteMatter": {
-        "filename": "https://raw.githack.com/rjrowekamp/NeuroMynerva/master/src/widgets/neu3d-widget/white_matter_50_5-69.json",
-        "label": "WhiteMatter",
+    "WhiteMatter C1": {
+        "filename": "https://raw.githack.com/rjrowekamp/NeuroMynerva/master/src/widgets/neu3d-widget/Inner_C1_50.json",
+        "label": "WhiteMatter C1",
         "highlight": false,
         "background": true,
         "color": [
@@ -13,9 +13,97 @@ export var SpinalcordMesh = {
             0.15
         ]
     },
-    "GrayMatter": {
-        "filename": "https://raw.githack.com/rjrowekamp/NeuroMynerva/master/src/widgets/neu3d-widget/gray_matter_50_5-69.json",
-        "label": "GrayMatter",
+    "WhiteMatter C2": {
+        "filename": "https://raw.githack.com/rjrowekamp/NeuroMynerva/master/src/widgets/neu3d-widget/Inner_C2_50.json",
+        "label": "WhiteMatter C2",
+        "highlight": false,
+        "background": true,
+        "color": [
+            0.15,
+            0.01,
+            0.15
+        ]
+    },
+    "WhiteMatter C3": {
+        "filename": "https://raw.githack.com/rjrowekamp/NeuroMynerva/master/src/widgets/neu3d-widget/Inner_C3_50.json",
+        "label": "WhiteMatter C3",
+        "highlight": false,
+        "background": true,
+        "color": [
+            0.15,
+            0.01,
+            0.15
+        ]
+    },
+    "WhiteMatter C4": {
+        "filename": "https://raw.githack.com/rjrowekamp/NeuroMynerva/master/src/widgets/neu3d-widget/Inner_C4_50.json",
+        "label": "WhiteMatter C4",
+        "highlight": false,
+        "background": true,
+        "color": [
+            0.15,
+            0.01,
+            0.15
+        ]
+    },
+    "WhiteMatter C5": {
+        "filename": "https://raw.githack.com/rjrowekamp/NeuroMynerva/master/src/widgets/neu3d-widget/Inner_C5_50.json",
+        "label": "WhiteMatter C5",
+        "highlight": false,
+        "background": true,
+        "color": [
+            0.15,
+            0.01,
+            0.15
+        ]
+    },
+    "GrayMatter C1": {
+        "filename": "https://raw.githack.com/rjrowekamp/NeuroMynerva/master/src/widgets/neu3d-widget/Outer_C1_50.json",
+        "label": "GrayMatter C1",
+        "highlight": false,
+        "background": true,
+        "color": [
+            0.01,
+            0.25,
+            0.01
+        ]
+    },
+    "GrayMatter C2": {
+        "filename": "https://raw.githack.com/rjrowekamp/NeuroMynerva/master/src/widgets/neu3d-widget/Outer_C2_50.json",
+        "label": "GrayMatter C2",
+        "highlight": false,
+        "background": true,
+        "color": [
+            0.01,
+            0.25,
+            0.01
+        ]
+    },
+    "GrayMatter C3": {
+        "filename": "https://raw.githack.com/rjrowekamp/NeuroMynerva/master/src/widgets/neu3d-widget/Outer_C3_50.json",
+        "label": "GrayMatter C3",
+        "highlight": false,
+        "background": true,
+        "color": [
+            0.01,
+            0.25,
+            0.01
+        ]
+    },
+    "GrayMatter C4": {
+        "filename": "https://raw.githack.com/rjrowekamp/NeuroMynerva/master/src/widgets/neu3d-widget/Outer_C4_50.json",
+        "label": "GrayMatter C4",
+        "highlight": false,
+        "background": true,
+        "color": [
+            0.01,
+            0.25,
+            0.01
+        ]
+    },
+    "GrayMatter C5": {
+        "filename": "https://raw.githack.com/rjrowekamp/NeuroMynerva/master/src/widgets/neu3d-widget/Outer_C5_50.json",
+        "label": "GrayMatter C5",
         "highlight": false,
         "background": true,
         "color": [
