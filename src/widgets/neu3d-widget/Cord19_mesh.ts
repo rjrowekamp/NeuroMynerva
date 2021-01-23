d0d9a7e181fc2e9f9c4fd722526d4b36aa659ba4
rjrowekamp <rrowekamp@salk.edu>
2021-01-22 16:25:15 -0800

    Added Cord19 and Cord27.

---
+++ src/widgets/neu3d-widget/Cord19_mesh.ts
@@ -0,0 +1,27 @@
+/**
+ * Exports only the mesh required to render spinalcord
+ */
+export var Cord19Mesh = {
+    "GrayMatter": {
+        "filename": "https://raw.githack.com/rjrowekamp/NeuroMynerva/master/src/widgets/neu3d-widget/Cord19_gray_50.json",
+        "label": "GrayMatter",
+        "highlight": false,
+        "background": true,
+        "color": [
+            0.15,
+            0.01,
+            0.15
+        ]
+    },
+    "WhiteMatter": {
+        "filename": "https://raw.githack.com/rjrowekamp/NeuroMynerva/master/src/widgets/neu3d-widget/Cord19_white_50.json",
+        "label": "WhiteMatter",
+        "highlight": false,
+        "background": true,
+        "color": [
+            0.01,
+            0.25,
+            0.01
+        ]
+    }
+}
